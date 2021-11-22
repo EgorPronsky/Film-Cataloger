ebd33873f6eb16c607a16ec8a0da392a53f31451
Егор Пронский <groza.2002@mail.ru>
2021-11-22 22:56:35 +0300

    Film Form structure (without binding fields)

--- FilmCataloger/ViewModel/FilmFormViewModel.xaml.cs
+++ FilmCataloger/ViewModel/FilmFormViewModel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace FilmCataloger.ViewModel
 {
@@ -12,6 +13,39 @@ namespace FilmCataloger.ViewModel
         public FilmFormViewModel(long filmId)
         {
             InitializeComponent();
+        }
+
+        public void AddDirectorTextBox(object sender, RoutedEventArgs e)
+        {
+            Directors.Children.Add(GetConfiguredTextBox());
+        }
+        
+        public void AddWriterTextBox(object sender, RoutedEventArgs e)
+        {
+            Writers.Children.Add(GetConfiguredTextBox());
+        }
+        
+        public void AddProducerTextBox(object sender, RoutedEventArgs e)
+        {
+            Producers.Children.Add(GetConfiguredTextBox());
+        }
+        
+        public void AddComposerTextBox(object sender, RoutedEventArgs e)
+        {
+            Composers.Children.Add(GetConfiguredTextBox());
+        }
+
+        private TextBox GetConfiguredTextBox()
+        {
+            TextBox textBox = new TextBox();
+            
+            textBox.Width = 500;
+            textBox.Height = 35;
+            textBox.Name = "txtNumber";
+            textBox.HorizontalAlignment = HorizontalAlignment.Left;
+            textBox.Margin = new Thickness(0, 5, 0, 0);
+
+            return textBox;
         }
     }
 }
